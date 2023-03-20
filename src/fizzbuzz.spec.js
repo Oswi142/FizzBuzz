5bea869ad6b6bc31d6e8aaef0bf871ca79ad9e21
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-03-20 18:13:02 -0400

    Añadiendo funcionalidad en fizzbuzz

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -1,2 +1,14 @@
 import fizz from "./fizz.js";
 import buzz from "./buzz.js";
+
+describe("FizzBuzz", () => {
+    it("Debe retornar fizz si es 3", () => {
+      expect(fizz(3)).toEqual("fizz");
+    });
+});
+  
+describe("FizzBuzz", () => {
+it("Debe retornar buzz si es 5", () => {
+    expect(buzz(5)).toEqual("buzz");
+    });
+});
