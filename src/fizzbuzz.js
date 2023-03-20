7186477b33089fc087d55136777d1facaa23bc3e
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-03-20 19:43:43 -0400

    Agregamos la funcionalidad de fizzbuzz

---
+++ src/fizzbuzz.js
@@ -0,0 +1,8 @@
+function fizzbuzz(num)
+{
+    if(num%3==0 && num%5==0)
+    {
+        return "fizzbuzz";
+    }
+}
+export default fizzbuzz;
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -1,5 +1,6 @@
 import fizz from "./fizz.js";
 import buzz from "./buzz.js";
+import fizzbuzz from "./fizzbuzz.js";
 
 describe("FizzBuzz", () => {
     it("Debe retornar fizz si es 3", () => {
@@ -12,3 +13,4 @@ it("Debe retornar buzz si es 5", () => {
     expect(buzz(5)).toEqual("buzz");
     });
 });
+
